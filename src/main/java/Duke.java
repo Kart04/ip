bda9741bda8a1f3f6c32c676077851aaa3360b5b
Karthik <e1398790@u.nus.edu>
2025-08-24 14:56:18 +0800

    Add reply feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String reply = " ";
+        String line = "____________________________________________________________";
+        String bye = "Bye";
+        System.out.println(line);
         System.out.println("Hello! I'm Speed\n" + "What can I do for you?");
+        System.out.println(line);
+        Scanner in = new Scanner(System.in);
+        while(!reply.equalsIgnoreCase(bye)){
+            reply = in.nextLine();
+            System.out.println(reply);
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
